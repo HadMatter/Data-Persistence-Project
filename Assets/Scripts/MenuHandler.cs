00e78a0493e5af7065d9a345277ec9ffc8357ee6
HadMatter <matt.waits@gmail.com>
2023-07-09 12:05:15 -0500

    High Score Menu work
    + Exit button on main menu working
    + high score scene UI elements
    + created highscorehandler script

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor; // There is no UnityEditor when running standalone
+#endif
 
 public class MenuHandler : MonoBehaviour
 {
@@ -22,5 +25,14 @@ public class MenuHandler : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
