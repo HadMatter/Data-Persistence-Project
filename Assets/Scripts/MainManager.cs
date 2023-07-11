25cddc33fe8eb5141329df38b233c99e082bd7d3
HadMatter <matt.waits@gmail.com>
2023-07-11 00:07:34 -0500

    Begin Extras
    + brick repop basically working
       bricks currently spawn with ball still
       in play, going to try making score
       static and reloading scene to see if
       score carries over

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -19,25 +19,12 @@ public class MainManager : MonoBehaviour
     
     private bool m_GameOver = false;
 
+    private int brickCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
-        }
-
+        SetTheBoard();
         DataCenter.Instance.LoadGame();
         highScoreText.text = "Best Score : " + DataCenter.Instance.highScoreHolder + " : " + DataCenter.Instance.highScore;
     }
@@ -57,7 +44,8 @@ public class MainManager : MonoBehaviour
                 Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
             }
         }
-        else if (m_GameOver)
+        
+        if (m_GameOver)
         {
             if (score > DataCenter.Instance.highScore)
             {
@@ -67,17 +55,43 @@ public class MainManager : MonoBehaviour
             }
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                //m_GameOver = false;
-                //m_Started = false;
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
+
+        // Keep the game going if you clear all the bricks
+        if (brickCount <= 0 )
+            SetTheBoard();
+    }
+
+    private void SetTheBoard()
+    {
+        // If I understand this correctly...
+        // the lateral (x) scale of each brick is 0.55
+        // step, at 0.6, sets the spacing with 0.05 b/w each brick
+        const float step = 0.6f; 
+        int perLine = Mathf.FloorToInt(4.0f / step);
+        Debug.Log("perLine = " + perLine);
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        for (int i = 0; i < LineCount; ++i)
+        {
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint); // Ah, HERE'S where this listener is set
+            }
+        }
+        brickCount = LineCount * perLine;
     }
 
     void AddPoint(int point)
     {
         score += point;
         ScoreText.text = $"Score : {score}"; //???????????????
+        --brickCount;
     }
 
     public void GameOver()
